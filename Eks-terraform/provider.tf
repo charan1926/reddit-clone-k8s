100ef0be89298adc498a6e3c8b81cafd0fc9ef29
charan1926 <73768584+charan1926@users.noreply.github.com>
2025-01-31 17:13:11 +0530

    Update provider.tf

--- Eks-terraform/provider.tf
+++ Eks-terraform/provider.tf
@@ -1,13 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
 provider "aws" {
-  region = "ap-south-1"
+  access_key = "admin"
+  secret_access_key = "admin123"
+  region = "us-east-1"
+  s3_force_path_style = true
+  endpoints {
+    s3 = "http://192.168.56.10:9101"  # MinIO endpoint
+  }
 }
