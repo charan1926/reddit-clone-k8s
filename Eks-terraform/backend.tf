7d8b008c2c91bb55164813ee597fed8666d00168
charan1926 <73768584+charan1926@users.noreply.github.com>
2025-01-31 15:36:40 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "reddit"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    endpoint       = "http://127.0.0.1:9000"
+    endpoint       = "http://192.168.56.10:9101"
     access_key     = "admin"
     secret_key     = "admin123"
     skip_credentials_validation = true
