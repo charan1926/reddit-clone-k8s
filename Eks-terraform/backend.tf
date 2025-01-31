6a153bcba77677e00dbb2a86781c5653aadeb985
charan1926 <73768584+charan1926@users.noreply.github.com>
2025-01-31 15:03:23 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,13 @@
 terraform {
   backend "s3" {
-    bucket = "charan-s" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
-    region = "eu-north-1"
+    bucket         = "reddit"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    endpoint       = "http://127.0.0.1:9000"
+    access_key     = "admin"
+    secret_key     = "admin123"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    force_path_style            = true
   }
 }
