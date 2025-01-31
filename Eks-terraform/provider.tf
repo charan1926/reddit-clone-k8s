7eecd39eafe038fdd0fc7f620c130b3a24c7863c
charan1926 <73768584+charan1926@users.noreply.github.com>
2025-01-31 20:18:32 +0530

    Update provider.tf

--- Eks-terraform/provider.tf
+++ Eks-terraform/provider.tf
@@ -1,9 +1,11 @@
 provider "aws" {
-  access_key = "admin"
-  secret_access_key = "admin123"
-  region = "us-east-1"
-  s3_force_path_style = true
-  endpoints {
-    s3 = "http://192.168.56.10:9101"  # MinIO endpoint
+  access_key                  = "admin"
+  secret_key                  = "admin123"
+  region                      = "us-east-1"
+  s3_force_path_style         = true
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  endpoints = {
+    s3 = "http://192.168.56.10:9101" # MinIO Endpoint
   }
 }
