4cb7132f772c96e23a27b0823a8ede550a9b1867
charan1926 <73768584+charan1926@users.noreply.github.com>
2025-01-31 18:27:01 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,11 +1,13 @@
 terraform {
   backend "s3" {
-    bucket         = "reddit"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    endpoint       = "http://192.168.56.10:9101"
-    access_key     = "admin"
-    secret_key     = "admin123"
+    endpoints = {
+      s3 = "http://192.168.56.10:9101" # Your MinIO endpoint
+    }
+    bucket                      = "my-minio-bucket"
+    key                         = "terraform/state"
+    region                      = "us-east-1" 
+    access_key                  = "your-minio-access-key"
+    secret_key                  = "your-minio-secret-key"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     force_path_style            = true
